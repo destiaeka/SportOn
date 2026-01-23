97d101a595f9bc432f7073dffc96cfb175b90ec3
destiaaekaa <destiaeka38@gmail.com>
2026-01-23 21:12:41 +0700

    integration dashboard admin | categories

--- app/(dashboard)/components/categories/category-table.tsx
+++ app/(dashboard)/components/categories/category-table.tsx
@@ -1,21 +1,16 @@
+import { getImageUrl } from "@/app/lib/api";
+import { Category } from "@/app/types";
 import priceFormatter from "@/utils/price-formatter";
 import Image from "next/image";
 import { FiEdit2, FiTrash2 } from "react-icons/fi";
 
-const categoryData = [
-    {
-        name: "Running",
-        imageUrl: "/images/category/category-running.svg",
-        description: "lorem ipsum"
-    },
-    {
-        name: "Badminton",
-        imageUrl: "/images/category/category-badminton.svg",
-        description: "lorem ipsum"
-    }
-]
+type TCategorytableProps = {
+    categories: Category[]
+    onEdit: (category: Category) => void
+    onDelete: (id: string) => void
+}
 
-const CategoryTable = () => {
+const CategoryTable = ({categories, onDelete, onEdit}: TCategorytableProps) => {
     return (
         <div className="bg-white rounded-xl border-gray-200">
             <table className="w-full text-left boder-collapse">
@@ -28,23 +23,23 @@ const CategoryTable = () => {
                 </thead>
                 <tbody>
                     {
-                        categoryData.map((data, index) => (
+                        categories.map((data, index) => (
                             <tr key={index} className="border-b border-gray-200 last:border-b-0">
                                 <td className="px-6 py-4 font-medium">
                                     <div className="flex gap-2">
                                         <div className="aspect-square bg-gray-100 rounded-md">
-                                            <Image src={data.imageUrl} width={52} height={52} alt={data.name} className="aspect-square object-contain"/>
+                                            <Image src={getImageUrl(data.imageUrl)} width={52} height={52} alt={data.name} className="aspect-square object-contain"/>
                                         </div>
                                         <span>{data.name}</span>
                                     </div>
                                 </td>
                                 <td className="px-6 py-4 font-medium">{data.description}</td>
                                 <td className="items-center px-7 py-7.5 flex gap-3 text-gray-600">
-                                    <button>
-                                        <FiEdit2 size={20}/>
+                                    <button onClick={() => onEdit?.(data)} className="cursor-pointer">
+                                        <FiEdit2 size={20} className="cursor-pointer"/>
                                     </button>
-                                    <button>
-                                        <FiTrash2 size={20}/>
+                                    <button onClick={() => onDelete?.(data._id)} className="cursor-pointer">
+                                        <FiTrash2 size={20} className="cursor-pointer"/>
                                     </button>
                                 </td>
                             </tr>
